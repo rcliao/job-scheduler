7eeed977134da001deffaeffbdccaa237901d376
Eric Liao <rcliao01@gmail.com>
2017-07-12 15:20:28 -0700

    chore: add more todo comments

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # job-scheduling
+
 A simulation server to simulate work with webhook
+
+## Get started
+
+Before starting the applications server, you will need Kala server running ahead
+of time. Please start the Kala server and pass the Kala server address through
+environment variable `SCHEDULER_SERVER_URL`.
+
+And after that, please run main.go to start the job simulation server!
--- main.go
+++ main.go
@@ -10,35 +10,59 @@ import (
 	"github.com/ajvb/kala/job"
 )
 
-func jobHandler(c *client.KalaClient) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		createJob(c)
-	})
+// Manager is the basic unit to represent consumer to maintain their own worker
+type Manager struct {
+	ID      string
+	Name    string
+	Workers []Worker
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Hello")
+// Worker represents the base simulation unit with efficiency
+type Worker struct {
+	Manager      Manager
+	Efficienct   int
+	Name         string
+	CurrentJobID string
 }
 
 func main() {
+	// TODO: get the schedudler server url from environment variable
 	c := client.New("http://127.0.0.1:8000")
+
+	// TODO: maintain a list of resources
+
+	// TODO: create a list of workers each with different efficiency
+
 	http.HandleFunc("/job", jobHandler(c))
 	http.HandleFunc("/hello", helloHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func createJob(c *client.KalaClient) {
+func jobHandler(c *client.KalaClient) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// TODO: parse the callback URL
+		url := "127.0.0.1:8080/hello"
+		name := "test_job"
+		createJob(c, url, name)
+	})
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello job simulation server")
+	r.Println("Hello job simulation server")
+}
+
+func createJob(c *client.KalaClient, url, name string) {
 	// construct the current time string
 	delay := rand.Int31n(60)
 	t := time.Now().
 		Add(time.Duration(delay) * time.Second).
 		Format("2006-01-02T15:04:05-07:00")
 	schedule := fmt.Sprintf("R2/%s/PT1S", t)
-	fmt.Println(schedule)
 	body := &job.Job{
 		Schedule: schedule,
-		Name:     "test_job",
-		Command:  "curl 127.0.0.1:8080/hello",
+		Name:     name,
+		Command:  "curl " + url,
 	}
 	id, err := c.CreateJob(body)
 	if err != nil {
